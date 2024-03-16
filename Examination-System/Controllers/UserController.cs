bbc21907aa6f1fb778f6d565eafae49050be5844
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-03-16 21:50:39 +0200

    connecting to the database and create the first user controller

---
+++ Examination-System/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Examination_System.Repos;
+
+namespace Examination_System.Controllers
+{
+    public class UserController : Controller
+    {
+        IUserRepo userRepo; //user repository
+
+        public UserController(IUserRepo _userRepo) //constructor
+        {
+            userRepo = _userRepo;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (userRepo.IsUserCredentialsValid(username, password).Result) //check if the user credentials are valid
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid Credentials";
+                return View();
+            }
+        }
+    }
+}
