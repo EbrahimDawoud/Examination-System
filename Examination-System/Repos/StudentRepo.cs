9a7ca202553b4b7f0468478ecc0360e43586a887
abdelhameed osama <60205598+AbdelhameedOsama@users.noreply.github.com>
2024-03-24 22:01:24 +0200

    Fix exam (#25)
    * Fix authorization attribute and add timer to TakeExam view
    
    * dashBoardImprovements
    
    * dash
    
    ---------
    
    Co-authored-by: Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>

--- Examination-System/Repos/StudentRepo.cs
+++ Examination-System/Repos/StudentRepo.cs
@@ -16,11 +16,12 @@ namespace Examination_System.Repos
         public Task<bool> IsStudentExamSubmitted(int examId, int studentId);
         public Task<List<StudentCourse>> GetStudentCourses(int id);
         public Task<List<StudentCourse>> GetStudentResultsByStdId(int stdId);
-        public Task<Exam> GetResultExam(int stdId , int crsId);
+        public Task<Exam> GetResultExam(int stdId, int crsId);
         public Task<List<string>> StudentAnswer(int examId, int studentId);
         public List<List<string>> ExamQuestionOptions(List<ExamQuestion> examQuestion);
         public List<string> ExamQuestionAnswers(List<ExamQuestion> examQuestion);
-
+        public Task MarkExam(int examId, int studentId);
+        public Task<User> GetUserById(int id);
 
     }
 
@@ -37,7 +38,14 @@ namespace Examination_System.Repos
         {
             try
             {
-                return await db.Exams.Where(e => e.CrsId == crsId).Include(c => c.Crs).Include(e => e.ExamQuestions).ThenInclude(q => q.Question).FirstOrDefaultAsync();
+                // return the last exam added to the course
+                return await db.Exams.Where(e => e.CrsId == crsId)
+                    .Include(c => c.Crs)
+                    .Include(e => e.ExamQuestions)
+                    .ThenInclude(q => q.Question)
+                    .ThenInclude(qo => qo.QuestionOptions)
+                    .OrderByDescending(e => e.ExamId)
+                    .FirstOrDefaultAsync();
             }
             catch (Exception ex)
             {
@@ -50,7 +58,13 @@ namespace Examination_System.Repos
         {
             try
             {
-                return await db.Exams.Where(e => e.ExamId == examId).Include(c => c.Crs).Include(e => e.ExamQuestions).ThenInclude(q => q.Question).ThenInclude(qo => qo.QuestionOptions).FirstOrDefaultAsync();
+                return await db.Exams.Where(e => e.ExamId == examId)
+                    .Include(c => c.Crs)
+                    .Include(e => e.ExamQuestions)
+                    .ThenInclude(q => q.Question)
+                    .ThenInclude(qo => qo.QuestionOptions)
+                    .OrderByDescending(e => e.ExamId)
+                    .FirstOrDefaultAsync();
             }
             catch (Exception ex)
             {
@@ -63,9 +77,18 @@ namespace Examination_System.Repos
         {
             //int grade = 0;
             //float finalGrade = db.ExamQuestions.Where(eq => eq.ExamId == studentAnswers[0].ExamId).Sum(eq => eq.Degree); //get the total degree of the exam
-            
+
             try
             {
+                //int courseID = await db.Exams.Where(e => e.ExamId == examId).Select(e => e.CrsId).FirstOrDefaultAsync();
+                //StudentCourse markedExam = await db.StudentCourses.Where(sc => sc.StudentId == studentId && sc.CrsId == courseID).FirstOrDefaultAsync();
+                //if (markedExam != null)
+                //{
+                //    db.StudentCourses.Remove(markedExam);
+
+                //    await db.SaveChangesAsync();
+                //}
+
                 foreach (var answer in studentAnswers)
                 {
                     //var questionGrade = await db.ExamQuestions.Where(eq => eq.ExamId == answer.ExamId && eq.QuestionId == answer.QuestionId).Select(eq => eq.Degree).FirstOrDefaultAsync(); //get the degree of the question
@@ -109,7 +132,7 @@ namespace Examination_System.Repos
                 return false;
             }
         }
-        
+
         public async Task<StudentCourse> GetStudentCourseDegree(int examId, int studentId) //get the student exam degree
         {
             try
@@ -148,9 +171,9 @@ namespace Examination_System.Repos
                 //};
 
                 //return await db.StudentCourses.FromSqlRaw("EXECUTE sp_Get_Cources_By_StudentId @stdId", parameters).ToListAsync();
-                
+
                 return await db.StudentCourses.Where(sc => sc.StudentId == id).Include(sc => sc.Crs).ToListAsync();
-              
+
             }
             catch (Exception ex)
             {
@@ -170,80 +193,110 @@ namespace Examination_System.Repos
                 return null;
             }
         }
-		public async Task<Exam> GetResultExam(int stdId , int crsId)
-		{
-			var StdExam = await db.StudentExams.FirstOrDefaultAsync(se => se.StdId == stdId);
-			if (StdExam != null)
-			{
-				int examId = StdExam.ExamId;
-                if(db.Exams.FirstOrDefault(s=>s.ExamId == examId).CrsId == crsId)
+        public async Task<Exam> GetResultExam(int stdId, int crsId)
+        {
+            var StdExam = await db.StudentCourses.FirstOrDefaultAsync(se => se.StudentId == stdId);
+            if (StdExam != null)
+            {
+                int examId = GetExamByCrsId(crsId).Result.ExamId;
+                if (db.Exams.FirstOrDefault(s => s.ExamId == examId).CrsId == crsId)
                 {
-					return await db.Exams.Where(e => e.ExamId == examId).Include(c => c.Crs).Include(e => e.ExamQuestions).ThenInclude(q => q.Question).ThenInclude(qo => qo.QuestionOptions).FirstOrDefaultAsync();
-				}
+                    return await db.Exams.Where(e => e.ExamId == examId).Include(c => c.Crs).Include(e => e.ExamQuestions).ThenInclude(q => q.Question).ThenInclude(qo => qo.QuestionOptions).FirstOrDefaultAsync();
+                }
                 else { return null; }
-			}
-			else
-			{
-				return null;
-			}
-		}
-		public async Task<List<string>> StudentAnswer(int examId, int studentId)
-		{
-			try
-			{
-				var answers =db.StudentAnswers.Where(sa => sa.ExamId == examId && sa.StudentId == studentId).ToListAsync();
-                Dictionary<int,int> answerOp= new Dictionary<int, int>();
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public async Task<List<string>> StudentAnswer(int examId, int studentId)
+        {
+            try
+            {
+                var answers = db.StudentAnswers.Where(sa => sa.ExamId == examId && sa.StudentId == studentId).ToListAsync();
+                Dictionary<int, int> answerOp = new Dictionary<int, int>();
                 foreach (var item in await answers)
                 {
-					answerOp.Add(item.QuestionId, item.SelectedOption);
-				}
-                
-                List<string>StdAnswers = new List<string>();
+                    answerOp.Add(item.QuestionId, item.SelectedOption);
+                }
+
+                List<string> StdAnswers = new List<string>();
                 foreach (var item in answerOp)
                 {
-                    StdAnswers.Add(db.QuestionOptions.Where(qo => qo.QuestionId ==item.Key &&qo.OptionNo == item.Value ).Select(qo => qo.OptionText).FirstOrDefault());
+                    StdAnswers.Add(db.QuestionOptions.Where(qo => qo.QuestionId == item.Key && qo.OptionNo == item.Value).Select(qo => qo.OptionText).FirstOrDefault());
                 }
                 return StdAnswers;
 
-			
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-				return null;
-			}
-		}
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
         public List<List<string>> ExamQuestionOptions(List<ExamQuestion> examQuestion)
         {
-			List<List<string>> myList = new List<List<string>>();
-			foreach (var item in examQuestion)
-			{
+            List<List<string>> myList = new List<List<string>>();
+            foreach (var item in examQuestion)
+            {
                 var i = 1;
-				List<string> options = new List<string>();
-				foreach (var option in item.Question.QuestionOptions)
-				{
+                List<string> options = new List<string>();
+                foreach (var option in item.Question.QuestionOptions)
+                {
                     if (option.OptionNo == 0) { i = 0; }
-					options.Add(item.Question.QuestionOptions.Where(qo => qo.QuestionId == item.QuestionId && qo.OptionNo==i).Select(qo => qo.OptionText).FirstOrDefault());
+                    options.Add(item.Question.QuestionOptions.Where(qo => qo.QuestionId == item.QuestionId && qo.OptionNo == i).Select(qo => qo.OptionText).FirstOrDefault());
                     i++;
-				}
-				myList.Add(options);
-			}
-			return myList;
-		}
+                }
+                myList.Add(options);
+            }
+            return myList;
+        }
         public List<string> ExamQuestionAnswers(List<ExamQuestion> examQuestion)
         {
             List<string> answers = new List<string>();
-			Dictionary<int, int> answerData = new Dictionary<int, int>();
-			foreach (var item in examQuestion)
+            Dictionary<int, int> answerData = new Dictionary<int, int>();
+            foreach (var item in examQuestion)
             {
                 answerData.Add(item.QuestionId, item.Question.QuestionAnswer);
-			}
+            }
             foreach (var item in answerData)
             {
                 answers.Add(db.QuestionOptions.Where(qo => qo.QuestionId == item.Key && qo.OptionNo == item.Value).Select(qo => qo.OptionText).FirstOrDefault());
             }
             return answers;
         }
-		
+
+        public async Task MarkExam(int examId, int studentId)
+        {
+            try
+            {
+                int CrsId = GetExamById(examId).Result.CrsId;
+                var markedExam = db.StudentCourses.Where(sc => sc.CrsId == CrsId && sc.StudentId == studentId).FirstOrDefault();
+                if (markedExam != null)
+                {
+                    markedExam.Grade = "0";
+
+                    await db.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public async Task<User> GetUserById(int id)
+        {
+			try
+            {
+				return await db.Users.Where(u => u.UserId == id).Include(s=>s.Student).ThenInclude(s=>s.StudentCourses).ThenInclude(s=>s.Crs).FirstOrDefaultAsync();
+			}
+			catch (Exception ex)
+            {
+				Console.WriteLine(ex.Message);
+				return null;
+			}
+        }
     }
 }
